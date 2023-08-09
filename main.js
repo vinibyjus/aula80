ee09b723e59d72dc10704ed02519dce4ea135969
vinibyjus <138735954+vinibyjus@users.noreply.github.com>
2023-08-09 17:20:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var guestname = document.getElementById("neme1").ariaValueMax;
+namesOfPeople.push(guestname);
+
+namesOfPeople.sort();
+
+function seaching()
+{
+    var s= document.getElementById("s1").Value;
+var found=0
+var j;
+for(j=0; j<namesOfPeople.length; j++)
+{
+    if(s==namesOfPeople[j]){
+  found=found+1;  
+}
+{
+document.getElementById("p2").innerHTML=nomeencontrado
+console.log("nome encontrato"+found"vez(es));
+{
